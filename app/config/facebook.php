0f6a149fb855b979b0c34051c4bd6b0e4af0e7d9
Luis Felipe Alvarez Burgos <falvarez@votainteligente.cl>
2011-09-26 15:09:26 -0300

    Changes Text: changes text for publishing the result of media naranja in facebook timeline
    Change-Id: I317b9f8536747ce4c6e78be147dfdd942d611a4c

--- app/config/facebook.php
+++ app/config/facebook.php
@@ -5,7 +5,7 @@ $config['Facebook']['APP_URL']	= 'http://apps.facebook.com/medianaranjatest/';
 $config['Facebook']['MEDIANARANJA']['form']['height'] = 3100;
 $config['Facebook']['MEDIANARANJA']['result']['height'] = 1790;
 $config['Facebook']['MEDIANARANJA']['wall']['name'] = 'Mi media naranja política es %s';
-$config['Facebook']['MEDIANARANJA']['wall']['caption'] = 'blablablabla';
+$config['Facebook']['MEDIANARANJA']['wall']['caption'] = 'La Media Naranja Política es una aplicación que permite ver tu afinidad con los candidatos a la pesidencia de la nación';
 $config['Facebook']['MEDIANARANJA']['wall']['action_link']['text'] = 'Fundación ciudadano inteligente';
 $config['Facebook']['MEDIANARANJA']['wall']['action_link']['href'] = 'http://www.ciudadanointeligente.cl/';
 ?>
